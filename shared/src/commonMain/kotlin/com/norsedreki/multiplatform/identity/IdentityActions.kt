e88f18abcc407441700a79694c02334f24cef5cc
Alexey Dmitriev <alexeydmitriev@cloud.upwork.com>
2023-05-25 14:00:19 +0300

    Add Identity actions

---
+++ shared/src/commonMain/kotlin/com/norsedreki/multiplatform/identity/IdentityActions.kt
@@ -0,0 +1,23 @@
+package com.norsedreki.multiplatform.identity
+
+sealed interface IdentityActions {
+
+    sealed interface WithPassword : IdentityActions {
+
+    }
+
+    sealed interface PasswordLess : IdentityActions {
+
+    }
+
+    sealed interface LogIn : IdentityActions {
+        data class WithPassword(val email: String, val password: String) : LogIn
+    }
+
+    data class SignUp(val name: String, val email: String, val confirmedPassword: String): IdentityActions
+
+    //Introduce Email class
+    data class ResetPassword(val forEmail: String): IdentityActions
+
+    object LogOut : IdentityActions
+}
